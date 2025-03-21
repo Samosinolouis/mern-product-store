df8534f51dcb8de47c2d550f31da35e0fc5b9db7
Samosinolouis <ljvsamosino@mymail.mapua.edu.ph>
2025-03-22 00:02:44 +0800

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import Navbar from "./components/navbar";
 
 function App() {
-  return <></>;
+  return (
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
